307b8d1490d26998ec971e059a7cdd7e6f33c518
Fawix28 <147719658+Fawix28@users.noreply.github.com>
2023-11-09 09:38:41 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
 import sys
 from hero import Hero
-<<<<<<< HEAD
 from enemy import Enemy
 import random
 
@@ -134,41 +133,4 @@ def start_game():
         pygame.display.flip()
         clock.tick(60)
 
-=======
-
-def start_game():
-    '''основная функция для описания игры'''
-    pygame.init()
-    screen = pygame.display.set_mode((600, 900))
-    pygame.display.set_caption("Space Invaders")
-
-    clock = pygame.time.Clock()
-    FPS = 60
-
-    hero = Hero(screen)
-
-    flag = True
-    while flag:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_d:
-                    hero.moving_right = True
-                if event.key == pygame.K_a:
-                    hero.moving_left = True
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_d:
-                    hero.moving_right = False
-                if event.key == pygame.K_a:
-                    hero.moving_left = False
-
-        hero.update()
-
-        screen.fill(0)
-        hero.output_hero()
-
-        pygame.display.flip()
-        clock.tick(FPS)
->>>>>>> edbfe5aa88db1b3582511ebef84b0dc3135fc0e1
 start_game()
