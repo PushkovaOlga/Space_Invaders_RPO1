b0d4d2238fcfe06a05020a501e51b08aeead888c
Fawix28 <147719658+Fawix28@users.noreply.github.com>
2023-11-14 11:52:56 +0300

    modified:   main.py

--- enemy.py
+++ enemy.py
@@ -1,19 +1,13 @@
 import pygame
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, screen):
-        super(Enemy, self).__init__()
-        self.screen = screen
+    def __init__(self, x, y):
+        super().__init__()
         self.image = pygame.image.load("Space_Invaders_RPO1_Chibotar/images/enemy.png")
         self.rect = self.image.get_rect()
-        self.rect.x = self.rect.width
-        self.rect.y = self.rect.height
-        self.x = float(self.rect.x)
-        self.y = float(self.rect.y)
-
-    def draw(self,screen):
-        self.screen.blit(self.image, self.rect)
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = 2
 
     def update(self):
-        self.y += 0.5
-        self.rect.y = self.y
+        self.rect.y += self.speed  
--- main.py
+++ main.py
@@ -1,25 +1,143 @@
-import pygame, controls
+import pygame
+import sys
 from hero import Hero
-from pygame.sprite import Group
+from enemy import Enemy
+import random
 
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.image.load("Space_Invaders_RPO1_Chibotar/images/bullet.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.speed = 5
+
+    def update(self):
+        self.rect.y -= self.speed
+
+    
 def start_game():
-    '''основная функция для описания игры'''
     pygame.init()
-    screen = pygame.display.set_mode((600, 900))
+    screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Самая лучшая игра")
-
-    #объекты классов
+    
+    background = pygame.image.load("Space_Invaders_RPO1_Chibotar/images/background.jpg")
+    
     hero = Hero(screen)
-    bullets = Group()
-    enemys = Group()
-
+    bullets = pygame.sprite.Group()
+    enemies = pygame.sprite.Group()
+    
+    clock = pygame.time.Clock()
+    spawn_interval = 1000  # Интервал в миллисекундах между спавном врагов
+    last_spawn_time = 0
     flag = True
-    while flag:
-        controls.events(screen, hero, bullets)
-        hero.moving_hero(screen)
 
-        controls.update(screen, hero, enemys, bullets)
-        controls.update_bullets(screen, bullets)
-        controls.create_army(screen,enemys)
+    acceleration = 0.2
+    deceleration = 0.1
+    max_speed = 5
+    speed_x = 0
+    speed_y = 0
+    
+    #ENEMY
+    max_enemies = 10
+    ENEMY_WIDTH = 50
+    ENEMY_HEIGHT = 50
+    SCREEN_WIDTH = 800
+    enemies = pygame.sprite.Group()
+
+    def spawn_enemy():
+        num_enemies = random.randint(1, 5)  # Случайное количество врагов от 1 до 5
+        for _ in range(num_enemies):
+            x = random.randint(0, SCREEN_WIDTH - ENEMY_WIDTH)
+            y = random.randint(-ENEMY_HEIGHT, 0)
+            enemy = Enemy(x, y)
+            enemies.add(enemy)
+            for bullet in bullets:
+                if pygame.sprite.spritecollide(bullet, enemies, True):
+                    bullets.remove(bullet)
+        enemies.draw(screen)
+    
+    
+    while flag:
+        hero.output_hero()   
+        if pygame.sprite.spritecollide(hero, enemies, True):
+            flag = False
+        screen.blit(background, (0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                flag = False
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    bullet = Bullet(hero.rect.centerx, hero.rect.top)
+                    bullets.add(bullet)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            speed_x -= acceleration
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            speed_x += acceleration
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            speed_y -= acceleration
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            speed_y += acceleration
+        
+        # Применяем замедление при отпускании клавиш
+        if not keys[pygame.K_a] and not keys[pygame.K_LEFT]:
+            if speed_x < 0:
+                speed_x += deceleration
+        if not keys[pygame.K_d] and not keys[pygame.K_RIGHT]:
+            if speed_x > 0:
+                speed_x -= deceleration
+        if not keys[pygame.K_w] and not keys[pygame.K_UP]:
+            if speed_y < 0:
+                speed_y += deceleration
+        if not keys[pygame.K_s] and not keys[pygame.K_DOWN]:
+            if speed_y > 0:
+                speed_y -= deceleration
+        
+        # Ограничиваем скорость
+        speed_x = max(-max_speed, min(max_speed, speed_x))
+        speed_y = max(-max_speed, min(max_speed, speed_y))
+
+        hero.rect.x += speed_x
+        hero.rect.y += speed_y
+        # Обновляем позицию пуль и удаляем пули, которые вышли за пределы экрана
+        for bullet in bullets:
+            bullet.update()
+            if bullet.rect.bottom < 0:
+                bullets.remove(bullet)
+                
+        
+        # Проверяем границы экрана
+        if hero.rect.left < 0:
+            hero.rect.left = 0
+        if hero.rect.right > screen.get_width():
+            hero.rect.right = screen.get_width()
+        if hero.rect.top < 0:
+            hero.rect.top = 0
+        if hero.rect.bottom > screen.get_height():
+            hero.rect.bottom = screen.get_height()
+        
+        for bullet in bullets:
+            screen.blit(bullet.image, bullet.rect)
+        
+        
+        current_time = pygame.time.get_ticks()
+
+        if current_time - last_spawn_time > spawn_interval:
+            spawn_enemy()
+            last_spawn_time = current_time
+
+        for enemy in enemies:
+            enemy.update()
+            screen.blit(enemy.image, enemy.rect)
+
+        hero.output_hero()  
+  
+        
+        pygame.display.flip()
+        clock.tick(50)
 
 start_game()
